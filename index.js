21f2179af2b02f87c48268f0cb6092522512a426
Romeu Neves <51343870+rnevesphp@users.noreply.github.com>
2024-02-22 08:16:49 +0100

    nova rota para adicionar informação

--- index.js
+++ index.js
@@ -11,4 +11,31 @@ app.get('/item' , (req , res) => {
     res.send(lista);
 })
 
+app.get( '/item/:id' , (req, res) => {
+    const id = req.params.id ; 
+
+    const itemDaLista = lista[id]
+
+    const numDeElementosNaLista =  lista.length;
+
+    id > numDeElementosNaLista || id < 0 ?  
+        res.status(400).send("ID inválido") : 
+        res.send(`${itemDaLista}`) 
+})
+
+
+app.post( '/adicionar' , (req , res) => {
+    const body = req.body
+    
+    console.log(body)
+    
+    res.send(body)
+   
+   
+    /*  !nomeDoItem ? 
+        "é preciso inserir um nome" : 
+        ((lista.push(nomeDoItem)) , (res.send(`Nome ${nomeDoItem} adicionado`))) */
+
+})
+
 app.listen(3000);
