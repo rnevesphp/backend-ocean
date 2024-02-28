9155fd3fb8ee23a9f2eb5a51587b523140d028fb
Romeu Neves <51343870+rnevesphp@users.noreply.github.com>
2024-02-28 14:24:06 +0100

    update by id and coding refactoring

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
 const express = require("express");
 const { MongoClient , ObjectId } = require("mongodb");
 
+const app = express();
+app.use(express.json());
+
+// credenciais do Mongo Db 
 const dbURL = "mongodb+srv://admin:AUwvkLdL85UEqEVV@cluster0.s5s27mc.mongodb.net";
 const dbName = "OceanDatabase2024";
 
+
 const main = async () => {
     const client = new MongoClient(dbURL);
-    console.log("Conectando banco de dados");
-    await client.connect();
-
-    const app = express();
-
-    /* const lista = ["Rick Sanchez", "Morty Smith", "Summer Smith"]; */
-
     const db = client.db(dbName)
     const collection = db.collection('items')
+    await client.connect();
 
+    console.log("DB CONECTADO"); 
+
+
+    // get all items from db
     app.get('/item' , async (req, res) => {
         const items = await collection.find().toArray()
 
         res.send(items)
     })  
 
-    /*    
-        app.get("/item",  (req, res) => {
-        res.send(lista);
-    }); */
-
+    // get by id from db 
     app.get("/item/:id", async (req, res) => {
         const id = req.params.id;
         const item = await collection.findOne({
@@ -35,8 +34,7 @@ const main = async () => {
         res.send(item);
     });
 
-    app.use(express.json());
-
+    // insert into the db 
     app.post("/item", async (req, res) => {
         const item = req.body;
         await collection.insertOne(item); 
@@ -45,7 +43,20 @@ const main = async () => {
         );
     });
 
-    app.listen(3000);
+    // update by id from db 
+    app.put('/item/:id' , async (req , res ) => {
+        const id = req.params.id; 
+        const novoItem = req.body;
+        
+        await collection.updateOne(
+
+            { _id : new ObjectId(id)}, 
+            { $set : novoItem }
+        )
+        res.send(`item ${id} atualizado com sucesso`);
+    })
 };
+
+app.listen(3000);
 
 main(); 
